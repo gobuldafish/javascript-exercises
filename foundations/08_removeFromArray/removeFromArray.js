0199b997a4268ee72e405fcc154159f209c4bd4c
Ethan <163362947+gobuldafish@users.noreply.github.com>
2026-09-06 15:04:25 -0400

    edit removeFromArray.js and removeFromArray.spec.js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // Go through array
+    // Filter out the items that don't match the arguments from the array
+    
+    return arr.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
