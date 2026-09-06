2821edcc51be90d27ac1c7f7cf0b1ed15c0711c0
Ethan <163362947+gobuldafish@users.noreply.github.com>
2026-09-06 14:22:52 -0400

    edit repeatString.js and repeatString.spec.js

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if(num < 0) {
+        return "ERROR"
+    }
+        //repeat the string given number of times
+        
+    let string = "";
+    for(let i = 0; i < num; i++) {
+        string += word;
+    }
+    return string;
+    
 };
 
 // Do not edit below this line
