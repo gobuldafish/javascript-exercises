5635d526f0e7e3ea2b7f171ba63ad039ab51a0bf
Ethan <163362947+gobuldafish@users.noreply.github.com>
2026-09-19 16:32:22 -0400

    add palindrome functionality

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    return string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '').split("").reverse().join("") === 
+    string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '') ? true : false;
 };
 
 // Do not edit below this line
