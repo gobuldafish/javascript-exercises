c00cafa95afb443bf4848112d0afdb0c96f20ad8
Ethan <163362947+gobuldafish@users.noreply.github.com>
2026-09-21 17:49:37 -0400

    add fibonacci functionality

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+   //if num is equal to 1 or 2, return 1
+   let total = 0;
 
+   if(Number(num) === 0) {
+    return 0;
+   } else if(Number(num) === 1 || Number(num) === 2) {
+    return 1;
+   } else if(Number(num < 0)) {
+    return 'OOPS';
+   }
+   let firstPrev = 0;
+   let secondPrev = 1;
+
+   for(let i = 0; i < num; i++) {
+    total = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = total;
+   }
+   return total;
 };
 
 // Do not edit below this line
